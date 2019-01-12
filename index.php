4bc716aa2065beb570cc063ffc74eab1b4dac067
Tristan Roux <tr.tristan-roux@outlook.fr>
2019-01-12 18:29:50 +0100

    Initial commit.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <title>MusicApp - RaccoonSDG</title>
+        <link rel="shortcut icon" type="image/png" href="drawables\icon.png"/>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link href=style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php include('navBar.php'); ?>
+    </body>
+</html>
---
+++ navBar.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-primary" >
+    <div class="collapse navbar-collapse" id="content" >
+        <ul class="navbar-nav">
+            <li class="navbar-item active">
+                <a href="\" class="nav-link">MusicApp</a>
+            </li>
+        </ul>
+    </div>
+</nav>'
