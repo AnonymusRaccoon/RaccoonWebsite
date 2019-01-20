542796022fc1100f49ec3962433dfa3e5972eb0e
Tristan Roux <tr.tristan-roux@outlook.fr>
2019-01-20 21:44:03 +0100

    Reworking screenshots js.

--- js/horizontalScrolling.js
+++ js/horizontalScrolling.js
@@ -7,7 +7,7 @@ sliders.forEach((slider) =>
     slider.addEventListener("mousedown", (eventArg) =>
     {
         sliding = true;
-        startX = eventArg.pageX;
+        startX = eventArg.pageX + slider.scrollLeft;
     });
 
     slider.addEventListener("mousemove", (eventArg) =>
@@ -15,7 +15,14 @@ sliders.forEach((slider) =>
         if (sliding)
         {
             eventArg.preventDefault();
-            slider.scrollLeft += (startX - eventArg.pageX) * $(window).width() / (2 * slider.scrollWidth);
+            slider.scrollLeft = startX - eventArg.pageX;
+
+            if (slider.scrollLeft == 0)
+                document.getElementById("left-scroll").style.display = "none";
+            else
+                document.getElementById("left-scroll").style.display = "inline-block";
+            
+            
         }
     });
     
@@ -26,4 +33,26 @@ sliders.forEach((slider) =>
     slider.addEventListener("mouseleave", () => {
         sliding = false;
     });
+
+    //Shouldn't do it here, but will say it's ok.
+    document.getElementById("left-scroll").onclick = function()
+    {
+        slider.scrollBy({ top: 0, left: -slider.offsetWidth * 0.66, behavior: "smooth" });
+        document.getElementById("right-scroll").style.display = "inline-block";
+
+        if (slider.scrollLeft == 0)
+            document.getElementById("left-scroll").style.display = "none";            
+    }
+
+    document.getElementById("right-scroll").onclick = function()
+    {
+        slider.scrollBy({ top: 0, left: slider.offsetWidth * 0.66, behavior: "smooth" });
+        document.getElementById("left-scroll").style.display = "inline-block";
+
+        console.log("Offset: " + slider.scrollWidth);
+        console.log("Scroll: " + slider.scrollLeft);
+
+        if (slider.scrollLeft == slider.scrollWidth)
+            document.getElementById("right-scroll").style.display = "none";
+    }
 });
